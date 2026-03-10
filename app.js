1052e4c4722d3557688a7bca556d7ac7cf113b08
Likhilesh <likbalpande@example.com>
2026-03-10 19:40:22 +0530

    basic server

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    console.log("App is running...");
+    res.json({ status: "App is running" });
+});
 
 app.get("/health", (req, res) => {
     console.log("Health check hit");
