995ff7c4ea4acdf77ace8adecbc552c0f6e5b48f
Likhilesh <likbalpande@example.com>
2026-04-12 23:58:57 +0530

    chore: update .gitignore and enhance response messages in app.js

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-    console.log("App is running...");
-    res.json({ status: "App is running" });
+    console.log("Express App is running...");
+    res.json({ status: "Express App is running!" });
 });
 
 app.get("/health", (req, res) => {
     console.log("Health check hit");
-    res.json({ status: "ok" });
+    res.json({ status: "Express health is good..." });
 });
 
 app.get("/hello", (req, res) => {
     console.log("Hello endpoint hit");
     console.log(`IP : ${req.headers["x-forwarded-for"] || req.socket.remoteAddress}`);
-    res.json({ message: "Hello, World!" });
+    res.json({ message: "Hello from Express App!" });
 });
 
 app.listen(PORT, () => {
