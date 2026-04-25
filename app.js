aaf42561aa9dd85c7f7b017daf4252f8fb701fc3
neuralcoder1 <dasloknath2005@gmail.com>
2026-04-25 19:36:30 +0530

    app file new changes

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     console.log("Express App is running...");
-    res.json({ status: "Express App is running!" });
+    res.json({ status: "Express App is running from last 5 days and we not stop it!" });
 });
 
 app.get("/health", (req, res) => {
     console.log("Health check hit");
-    res.json({ status: "Express health is good..." });
+    res.json({ status: "Express health is good and working with full mood..." });
 });
 
 app.get("/hello", (req, res) => {
     console.log("Hello endpoint hit");
     console.log(`IP : ${req.headers["x-forwarded-for"] || req.socket.remoteAddress}`);
-    res.json({ message: "Hello from Express App!" });
+    res.json({ message: "Hello All!" });
 });
 
 app.listen(PORT, () => {
